2014995c2dcd7ae44e045b3b87ab25b934d5c712
raju <raju@LAPTOP-TMQKKRA2>
2020-08-07 23:33:59 +0530

    commit

---
+++ ActiTime/src/test/java/com/actiTime/timeTrack/Demo.java
@@ -0,0 +1,13 @@
+package com.actiTime.timeTrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("success", true);
+	}
+}
